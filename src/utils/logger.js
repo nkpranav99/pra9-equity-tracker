103706cb7db15997b6d45838a6e8ad9d4c96e8ea
Pranav Naik <nk.pranav99@gmail.com>
2026-05-31 20:38:22 +0530

    Disabled pino-pretty in production to fix PM2 logs

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,16 +1,20 @@
 import pino from 'pino';
 import config from '../config.js';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const logger = pino({
   level: config.log.level,
-  transport: {
-    target: 'pino-pretty',
-    options: {
-      colorize: true,
-      translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
-      ignore: 'pid,hostname',
+  ...(!isProd && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'SYS:yyyy-mm-dd HH:MM:ss',
+        ignore: 'pid,hostname',
+      },
     },
-  },
+  }),
 });
 
 export default logger;
